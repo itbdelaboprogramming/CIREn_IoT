46309be00533e15a142450cac056200904077cca
Furqan Riansyah <putra.furqan12@gmail.com>
2025-04-23 16:16:22 +0700

    edit: add pinout include, led blink, and serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <pinout/devkitv1.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Constants
+const unsigned long BLINK_INTERVAL = 500;
 
+// Globals for non-blocking LED
+unsigned long previousMillis = 0;
+bool ledState = LOW;
+
+// === Function declarations ===
+void hardware_init();
+void led_blink();
+
+// === Setup ===
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  hardware_init();
 }
 
+// === Loop ===
 void loop() {
-  // put your main code here, to run repeatedly:
+  led_blink();
+  Serial.println("Hello, I'm ESP32 Devkit V1 built using Platform.io");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// === Definitions ===
+void hardware_init() {
+
+  // Setup serial communication
+  Serial.begin(115200);
+
+  // Setup onboard LED
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void led_blink() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= BLINK_INTERVAL) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+  }
 }
