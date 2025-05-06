538fddb87abc198ac50a51a9713636472004b938
Furqan Riansyah <putra.furqan12@gmail.com>
2025-05-06 22:11:17 +0700

    fix: add extendedId `or` operation and add error logging

--- src/drivers/canbus/Canbus.cpp
+++ src/drivers/canbus/Canbus.cpp
@@ -42,7 +42,7 @@ int Canbus::send() {
         if (ret != ESP_OK) {
             LOGE(TAG_CAN_LIB, "Error sending CAN message: %s", esp_err_to_name(ret));
         } else {
-            LOGI(TAG_CAN_LIB, "CAN message sent successfully.");
+            LOGI(TAG_CAN_LIB, "CAN message (Extended) sent successfully.");
         }
         
     } else {
@@ -51,30 +51,26 @@ int Canbus::send() {
         if (ret != ESP_OK) {
             LOGE(TAG_CAN_LIB, "Error sending CAN message: %s", esp_err_to_name(ret));
         } else {
-            LOGI(TAG_CAN_LIB, "CAN message sent successfully.");
+            LOGI(TAG_CAN_LIB, "CAN message (Standard) sent successfully.");
         }
     }
-
-    // LOGI(TAG_CAN_LIB, "CAN message sent: ID = 0x%03X, DLC = %d", frame.can_id, frame.can_dlc);
-    // for (int i = 0; i < frame.can_dlc; i++) {
-    //     LOGI(TAG_CAN_LIB, "Data[%d] = 0x%02X", i, frame.data[i]);
-    // }
 
     return ESP_OK;
 }
 
 int Canbus::requestId(uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5) {
     
-    this->macAddress[0] = c0;
-    this->macAddress[1] = c1;
-    this->macAddress[2] = c2;
-    this->macAddress[3] = c3;
-    this->macAddress[4] = c4;
-    this->macAddress[5] = c5;
+    this->macAddress[0] = 0x0;
+    this->macAddress[1] = 0x1;
+    this->macAddress[2] = 0x2;
+    this->macAddress[3] = 0x3;
+    this->macAddress[4] = 0x4;
+    this->macAddress[5] = 0x5;
 
-    uint16_t crc = calculateCrc(this->macAddress, 6);
+    uint32_t crc = calculateCrc(this->macAddress, 6);
+    LOGI(TAG_CAN_LIB, "CRC: %X", crc);
 
-    txHeader.extendedId = crc << 3 | 5; 
+    txHeader.extendedId = (crc << 3 | 5) | CAN_EFF_FLAG; 
     txHeader.dlc = 6; 
     txHeader.rtr = false; 
     txHeader.isExtended = true; 
@@ -87,17 +83,27 @@ int Canbus::requestId(uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4
     this->txMailbox[5] = this->macAddress[5];
 
     // Set Filter for the response message
+    mcp2515.reset();
     mcp2515.setFilterMask(MCP2515::MASK0, true, 0xFFFFFF); 
     mcp2515.setFilter(MCP2515::RXF0, true, (crc << 3 | 5) ); 
+    mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+    mcp2515.setNormalMode();
 
     this->send();
     
-    delay(10);
+    while(!mcp2515.checkReceive()) {
+        LOGI(TAG_CAN_LIB, "Waiting for response...");
+        delay(100); 
+        
+        this->send();
+    }
 
     can_frame frame;
     if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK) {
+        LOGI(TAG_CAN_LIB, "Successfully receive response message.");
+        LOGI(TAG_CAN_LIB, "Received ID: %d", frame.data[1] << 8 | frame.data[0]);
         this->deviceId = frame.data[1] << 8 | frame.data[0]; // Mask to get the ID
-        return this->deviceId; // Return the received ID
+        return ESP_OK; // Return the received ID
     }
 
     return ESP_FAIL; // Error in receiving ID
@@ -105,7 +111,7 @@ int Canbus::requestId(uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4
 
 }
 
-uint16_t Canbus::calculateCrc(uint8_t* data, int length) {
+uint32_t Canbus::calculateCrc(uint8_t* data, int length) {
     uint16_t crc = 0xFFFF; // Initial CRC value
 
     for (uint8_t i = 0; i < length; i++) {
@@ -129,7 +135,7 @@ void Canbus::setStandardId(uint16_t deviceId, uint16_t SensorId) {
     this->txHeader.isExtended = false;
 }
 void Canbus::setExtendedId(uint16_t deviceId, uint16_t SensorId) {
-    this->txHeader.extendedId = (deviceId << 16) | SensorId;
+    this->txHeader.extendedId = ((deviceId << 16) | SensorId) | CAN_EFF_FLAG;
     this->txHeader.isExtended = true;
 }
 
@@ -155,7 +161,7 @@ int Canbus::getRxDeviceId() {
 }
 
 void Canbus::setMessageheartbeat() {
-    this->txHeader.extendedId = this->deviceId << 8 | 0; // Set standard ID for heartbeat message
+    this->txHeader.extendedId = (this->deviceId << 8 | 0) | CAN_EFF_FLAG; // Set standard ID for heartbeat message
     this->txHeader.dlc = 1; // Set the data length code (DLC) to 8 bytes
     this->txHeader.rtr = false; // Set the remote transmission request (RTR) flag to false
     this->txHeader.isExtended = true; // Set the extended ID flag to false
