9219f435122567c269fb7aab1ff2caff632ca796
Furqan Riansyah <putra.furqan12@gmail.com>
2026-02-05 19:41:32 +0700

    feat: make implementation of ModbusMaster library

--- src/main_slave.cpp
+++ src/main_slave.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
-#include "drivers/modbus/cwt_sensor.h"
 #include "drivers/ciren_comm/ciren_comm.h"
 
-CWTSensor sensor(Serial2);
+#include <ModbusMaster.h>
+
+
+#define DE_RE_PIN 26
+ModbusMaster node;
 
 // Fill with master mac address
 uint8_t masterMac[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
@@ -11,52 +14,59 @@ CIREnSlave slave(5);
 unsigned long lastReadTime = 0;
 const unsigned long READ_INTERVAL = 1000;
 
+void preTransmit() {
+    digitalWrite(DE_RE_PIN, HIGH);   // enable transmit
+}
+
+void postTransmit() {
+    digitalWrite(DE_RE_PIN, LOW);    // enable receive
+}
+
 void setup() {
     Serial.begin(115200);
     Serial.println("CWT Sensor Starting...");
 
-    sensor.begin(4800);
+    pinMode(DE_RE_PIN, OUTPUT);
+    digitalWrite(DE_RE_PIN, LOW);           // start in receive mode
+    Serial2.begin(4800, SERIAL_8N1, 16, 17);
+    node.begin(1, Serial2);             // slave ID 0x01, stream = Serial2
+    node.preTransmission(preTransmit);
+    node.postTransmission(postTransmit);
 
     Serial.println("Sensor initialized");
 
-    if (slave.findMaster(masterMac) == ESP_OK) {
-        Serial.println("Connected to master");
-        slave.sendSlaveStatus();
-    } else {
-        Serial.println("Failed to connect to master");
-    }
+    // if (slave.findMaster(masterMac) == ESP_OK) {
+    //     Serial.println("Connected to master");
+    //     slave.sendSlaveStatus();
+    // } else {
+    //     Serial.println("Failed to connect to master");
+    // }
 }
 
 void loop() {
-    sensor.task();
-
     unsigned long currentTime = millis();
 
-    // Sensor Update
     if (currentTime - lastReadTime >= READ_INTERVAL) {
         lastReadTime = currentTime;
 
-        sensor.read_temperature();
-        sensor.read_vibration();
-    }
+        // Read Temperature
+        if (node.readHoldingRegisters(0x0000, 1) == ModbusMaster::ku8MBSuccess) {
+            float temp = (float)node.getResponseBuffer(0);
+            Serial.print("Temperature: ");
+            Serial.println(temp);
 
-    // Get & Print Temperature
-    if (sensor.is_temperature_ready()) {
-        float temp = sensor.get_temperature();
-        Serial.print("Temperature: ");
-        Serial.println(temp);
+            uint16_t tempData = (uint16_t)(temp * 10);
+            // slave.sendTemperature(tempData);
+        }
 
-        uint16_t tempData = (uint16_t)(temp * 10);
-        slave.sendTemperature(tempData);
-    }
+        // Read Vibration
+        if (node.readHoldingRegisters(0x0003, 1) == ModbusMaster::ku8MBSuccess) {
+            float vib = (float)node.getResponseBuffer(0);
+            Serial.print("Vibration: ");
+            Serial.println(vib);
 
-    // Get & Print Vibration
-    if (sensor.is_vibration_ready()) {
-        float vib = sensor.get_vibration();
-        Serial.print("Vibration: ");
-        Serial.println(vib);
-
-        uint16_t vibData = (uint16_t)(vib * 10);
-        slave.sendVibration(vibData);
+            uint16_t vibData = (uint16_t)(vib * 10);
+            // slave.sendVibration(vibData);
+        }
     }
 }
