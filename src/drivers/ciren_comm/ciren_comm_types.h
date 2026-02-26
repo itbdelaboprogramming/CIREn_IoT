ffd78eeb4dcaf75a1948a4a154617f4c50a347c6
Furqan Riansyah <putra.furqan12@gmail.com>
2026-02-27 03:43:15 +0700

    feat: add velocity, displacement, acceleration, frequency metric types

--- src/drivers/ciren_comm/ciren_comm_types.h
+++ src/drivers/ciren_comm/ciren_comm_types.h
@@ -2,9 +2,13 @@
 
 #include <Arduino.h>
 
-#define METRIC_TYPE_SYSTEM 0
-#define METRIC_TYPE_TEMPERATURE 1
-#define METRIC_TYPE_VIBRATION 2
+#define METRIC_TYPE_SYSTEM       0
+#define METRIC_TYPE_TEMPERATURE  1
+#define METRIC_TYPE_VIBRATION    2
+#define METRIC_TYPE_VELOCITY     3
+#define METRIC_TYPE_DISPLACEMENT 4
+#define METRIC_TYPE_ACCELERATION 5
+#define METRIC_TYPE_FREQUENCY    6
 
 #define CMD_TURN_OFF 0
 #define CMD_TURN_ON 1
@@ -15,7 +19,8 @@ struct CIREnMessage {
     uint8_t id;
     uint8_t metric_type;
     union {
-        uint8_t system_data;
+        uint8_t  system_data;
         uint16_t sensor_data;
+        uint32_t frequency_data;
     } data;
 } __attribute__((packed));
